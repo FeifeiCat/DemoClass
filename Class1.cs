5f4e74e92c9634657c9b974e4e276ddaa7bed847
FeiFeiCat <18126522282@163.com>
2016-02-04 02:32:56 +0800

    Project-DemoClass upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoClass
+{
+    public class Class1
+    {
+
+        public int Sum(params int[] args)
+        {
+            int result = 0;
+            foreach (int item in args)
+            {
+                result += item;
+            }
+            return result;
+        }
+    }
+}
